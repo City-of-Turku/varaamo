89561f16c337a85d116fa73b61f30454d1909203
Santtu Alatalo <santtualatalo@gmail.com>
2021-12-01 14:48:28 +0200

    Refactored reservation payment modal

--- app/shared/modals/reservation-payment/PaymentModalContainer.js
+++ app/shared/modals/reservation-payment/PaymentModalContainer.js
@@ -2,16 +2,18 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import { Button, Modal } from 'react-bootstrap';
+import classNames from 'classnames';
 
 import injectT from '../../../i18n/injectT';
 import { closeReservationPaymentModal } from 'actions/uiActions';
-import ModalWrapper from '../ModalWrapper';
 import paymentModalSelector from './PaymentModalSelector';
 import { putReservation } from 'actions/reservationActions';
 import { getPaymentReturnUrl } from 'utils/reservationUtils';
+import TimeRange from '../../time-range/TimeRange';
 
 function UnconnectedPaymentModalContainer({
-  actions, reservation, show, t
+  actions, contrast, fontSize, reservation, resource, show, t
 }) {
   const handleUpdateReservation = () => {
     const returnUrl = getPaymentReturnUrl();
@@ -27,31 +29,58 @@ function UnconnectedPaymentModalContainer({
     window.location = order.paymentUrl;
   }
 
+  if (!reservation || !reservation.order) {
+    return null;
+  }
+
   return (
-    <ModalWrapper
-      className="payment-modal"
-      onClose={actions.closeReservationPaymentModal}
+    <Modal
+      className={classNames('reservation-payment-modal', fontSize, contrast)}
+      onHide={actions.closeReservationPaymentModal}
       show={show}
-      title={t('ReservationPaymentModal.title')}
     >
-      <div>
-        <p>
-          {`maksa tämä varaus: ${reservation.id}?`}
+      <Modal.Header closeButton closeLabel={t('ModalHeader.closeButtonText')}>
+        <Modal.Title componentClass="h3">
+          {t('ReservationPaymentModal.title')}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p className="reservation-name">{resource.name}</p>
+        <div className="reservation-time">
+          <TimeRange begin={reservation.begin} end={reservation.end} />
+        </div>
+        <p className="reservation-price">{`${t('common.priceTotalLabel')}: ${reservation.order.price}€`}</p>
+        <p className="reservation-payment-notice">
+          {t('ReservationPaymentModal.onlinePaymentNotice')}
         </p>
-        <button
+      </Modal.Body>
+      <Modal.Footer>
+        <Button
+          bsStyle="default"
+          className={fontSize}
+          onClick={actions.closeReservationPaymentModal}
+        >
+          {t('common.back')}
+        </Button>
+
+        <Button
+          bsStyle="success"
+          className={fontSize}
           onClick={() => handleUpdateReservation()}
-          type="button"
         >
           {t('common.proceedToPayment')}
-        </button>
-      </div>
-    </ModalWrapper>
+        </Button>
+      </Modal.Footer>
+    </Modal>
   );
 }
 
 UnconnectedPaymentModalContainer.propTypes = {
   actions: PropTypes.object.isRequired,
+  contrast: PropTypes.string.isRequired,
+  fontSize: PropTypes.string.isRequired,
   reservation: PropTypes.object.isRequired,
+  resource: PropTypes.object.isRequired,
   show: PropTypes.bool.isRequired,
   t: PropTypes.func.isRequired,
 };
--- app/shared/modals/reservation-payment/PaymentModalSelector.js
+++ app/shared/modals/reservation-payment/PaymentModalSelector.js
@@ -5,6 +5,7 @@ import { createSelector, createStructuredSelector } from 'reselect';
 
 import { createResourceSelector } from 'state/selectors/dataSelectors';
 import modalIsOpenSelectorFactory from 'state/selectors/factories/modalIsOpenSelectorFactory';
+import { contrastSelector, fontSizeSelector } from 'state/selectors/accessibilitySelectors';
 
 function reservationSelector(state) {
   return state.ui.reservations.toShow[0] || state.ui.reservations.toShowEdited[0] || {};
@@ -16,6 +17,8 @@ const resourceIdSelector = createSelector(
 );
 
 const paymentModalSelector = createStructuredSelector({
+  contrast: contrastSelector,
+  fontSize: fontSizeSelector,
   reservation: reservationSelector,
   resource: createResourceSelector(resourceIdSelector),
   show: modalIsOpenSelectorFactory(ModalTypes.RESERVATION_PAYMENT),
