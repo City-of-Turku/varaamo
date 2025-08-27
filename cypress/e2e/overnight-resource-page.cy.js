50095072c42cdb70e74310bbe377911e2f72b471
Markus Hällfors <markus.hallfors@gofore.com>
2025-08-27 14:02:13 +0300

    Add increased defaultCommandTimeout to overnight tests

--- cypress/e2e/overnight-resource-page.cy.js
+++ cypress/e2e/overnight-resource-page.cy.js
@@ -6,6 +6,7 @@ const { getReservationBeginEnd, getClosedDate } = require('../utils/scripts');
 
 describe('Overnight calendar', () => {
   beforeEach(() => {
+    Cypress.config('defaultCommandTimeout', 20000);
     cy.fixture('resource_overnight_detail.json').as('resourceDetail');
     cy.fixture('reservation_overnight.json').as('reservationOvernight');
   });
